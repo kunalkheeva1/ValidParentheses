cf0e80816ae3bb1ce692b6c99c7f61e3df0e00bb
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-19 22:22:06 -0700

    there are several other test cases that are not being matched, so lets do it with the stack way, I am going to delete the whole code now.

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -1,17 +1,20 @@
 public class ValidParentheses {
     public static boolean validParentheses(String str){
-        for(int i=0; i<str.length(); i++){
-            if(str.contains("[")&&str.contains("]")){
-                return true;
-            }else if(str.contains("{")&&str.contains("}")){
-                return true;
-            }else if (str.contains("(")&&str.contains(")")){
+        char [] first = str.toCharArray();
+
+        for(int i= 0; i<first.length; i++){
+           if(first.length%2!=0){
+               return false;
+           }
+            if((first[i]=='['&&first[i+1]==']')||(first[i]=='{'&&first[i+1]=='}')||(first[i]=='('&&first[i+1]==')')){
                 return true;
             }
         }return false;
+
     }
+
     public static void main(String[] args) {
-        System.out.println(  validParentheses("({)"));
+        System.out.println(  validParentheses("[){"));
 
     }
 }
