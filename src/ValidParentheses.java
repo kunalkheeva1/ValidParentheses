a1ed8567639fd16641a881186d32cace5831c1db
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-19 23:28:24 -0700

    working fine with the stacks but the only issue im having rn is the return s.isempty because thats true, and according to the default value of the boolean it should return false.

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -1,20 +1,43 @@
+import java.util.Stack;
+
 public class ValidParentheses {
-    public static boolean validParentheses(String str){
-        char [] first = str.toCharArray();
 
-        for(int i= 0; i<first.length; i++){
-           if(first.length%2!=0){
-               return false;
-           }
-            if((first[i]=='['&&first[i+1]==']')||(first[i]=='{'&&first[i+1]=='}')||(first[i]=='('&&first[i+1]==')')){
-                return true;
-            }
-        }return false;
-
+//creating a boolean method to check chars of my string
+    public boolean isMatching(char a, char b){
+       return (a=='(' && b==')') ||(a=='{' && b=='}')||(a=='[' && b==']');
     }
+    //method to confirm it
+    public boolean validParentheses(String str) {
 
+        //first crate a stack to store the values
+        Stack<Character> s = new Stack<>();
+        //run a loop to the end char fo str.
+        for(int i=0; i<str.length(); i++){
+            //current char will be at i
+            char current = str.charAt(i);
+            //if it is opening bracket, then just push it to the stack for further confirmation
+            if(current == '(' || current == '{' || current == '['){
+                s.push(current);
+            }
+            //now, check if the stack is empty, where we are storing the current then return flase
+            else if(s.empty()){
+                return false;
+            }
+            //if they dont match then just return false, else pop the element out of the stack
+            else if(!isMatching(s.peek(),current)){
+                return false;
+            }
+            //if matches, pop the stored element out of the stack
+            else{
+                s.pop();
+            }
+            //and my empty stack will be returned,,,
+        }return s.isEmpty();
+    }
     public static void main(String[] args) {
-        System.out.println(  validParentheses("[){"));
+        ValidParentheses kk = new ValidParentheses();
+        System.out.println( kk.validParentheses("{(())}"));
+
 
     }
 }
